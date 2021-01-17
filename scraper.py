26a877bb1e8d5fad0c938770f40cfa338f27739b
Brett E <edwards.brett@outlook.com>
2021-01-17 01:08:30 -0800

    init gunicorn

--- scraper.py
+++ scraper.py
@@ -79,4 +79,5 @@ def index():
 
     return render_template('index.html', **locals())
 
-# app.run()
+if __name__=='__main__':
+    app.run()
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from app.scraper import app 
-
-if __name__ == "__main__": 
-		app.run() 
